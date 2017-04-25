e2aafdccdf5bd9cc829459f99b4976ebcaa73cff
tnlthanzeel <tnlthanzeel@gmail.com>
2017-04-25 23:17:31 +0530

    reg2.aspx

--- WebApplication5/Registration2.aspx.cs
+++ WebApplication5/Registration2.aspx.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Data.SqlClient;
-using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 
 namespace lankapolitalks
 {
@@ -15,20 +9,20 @@ namespace lankapolitalks
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         protected void Button1_Click(object sender, EventArgs e)
         {
             try
             {
-                
-               
+
+
                 SqlConnection conn = new SqlConnection("Data Source=DESKTOP-LRBQLGE;Initial Catalog=lankanpolitalks;Integrated Security=True");
                 SqlCommand cmd = new SqlCommand();
                 cmd.CommandType = CommandType.Text;
                 cmd.Connection = conn;
-                SqlDataAdapter ada = new SqlDataAdapter() ;
+                SqlDataAdapter ada = new SqlDataAdapter();
                 ada.SelectCommand = cmd;
 
                 conn.Open();
@@ -37,21 +31,21 @@ namespace lankapolitalks
                 string pwd = txtPassword.Value;
 
 
-                cmd.CommandText = @"select [Password] from login where [Email] ='"+em+"'";
+                cmd.CommandText = @"select [Password] from login where [Email] ='" + em + "'";
                 DataTable dt = new DataTable();
                 ada.Fill(dt);
                 string password = dt.Rows[0]["Password"].ToString();
 
-                if ( pwd.Equals(password))
+                if (pwd.Equals(password))
                 {
                     Response.Redirect("index.html");
                 }
                 else
 
                 {
-                    
+
                     Response.Write("");
-                
+
                 }
 
                 //cmd.ExecuteNonQuery();
